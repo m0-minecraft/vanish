6b2fb96048eecab9c3e0557fcae2a14b4d146079
de-kam <106482443+de-kam@users.noreply.github.com>
2024-11-30 17:51:17 -0800

    Make 1st version

--- m0Vanish.lua
+++ m0Vanish.lua
@@ -12,11 +12,16 @@ function Initialize(Plugin)
   -- Command Bindings
 
   cPluginManager.BindCommand("/vanish", "m0Vanish.perm", Vanish, "Sets you in spectator and shows that you are not online/left")
+  cPluginManager.BindCommand("/v", "m0Vanish.perm", Vanish, "/vanish but shorter command name")
   return true
 end
 
 function Vanish(Split, Player)
-  Player:SetGameMode(3)
+  if Player:IsGameModeSpectator == true then
+    Player:SetGameMode(1)
+  else
+    Player:SetGameMode(3)
+  end
   return true
 end
 
